09ed18b80c0be8fda989b76a209ea94e15ad1741
solhwi <psh50zmfhtm@naver.com>
2024-05-10 00:53:44 +0900

    캐릭터 스폰 테스트 작업

--- SkeletonGame/Assets/Scripts/Editor/DevHelperWindow.cs
+++ SkeletonGame/Assets/Scripts/Editor/DevHelperWindow.cs
@@ -3,25 +3,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using static Define;
 
-public struct MenuInfo
+public struct ButtonInfo
 {
-	public readonly string name;
 	public readonly Action onClickMenu;
+	public readonly GUIStyle style;
 
-	public MenuInfo(string name, Action onClickMenu)
+	public ButtonInfo(Action onClickMenu, GUIStyle style)
 	{
-		this.name = name;
 		this.onClickMenu = onClickMenu;
+		this.style = style;
 	}
 }
 
 public class DevHelperWindow : EditorWindow
 {
-	private static List<MenuInfo> menuInfos = new List<MenuInfo>()
+	public enum Axis
 	{
-		new MenuInfo("테스트 버튼", OnClickTest)
-	};
+		Horizontal, 
+		Vertical,
+	}
+
+	public enum FieldType
+	{
+		String,
+		Float,
+		Int,
+	}
+
+	private static float dpi = 0.0f;
+
+	private Vector3 playerSpawnPos = Vector3.zero;
+	private int playerId = -1;
+	private Player currentPlayer = null;
 
 	[MenuItem("Tools/DevHelperWindow %#D")]
 	public static void OpenWindow()
@@ -33,29 +48,166 @@ public class DevHelperWindow : EditorWindow
 		}
 	}
 
+	private void OnEnable()
+	{
+		dpi = Screen.dpi;
+	}
+
+	private void OnDisable()
+	{
+		
+	}
+
 	private void OnGUI()
 	{
-		EditorGUILayout.BeginHorizontal();
-		{
-			MainProc();
-		}
-		EditorGUILayout.EndHorizontal();
+		DrawAxis(Axis.Horizontal, DrawAll);
 	}
 
-	private void MainProc()
+	private void DrawAll()
 	{
-		foreach (var menuInfo in menuInfos)
+		DrawAxis(Axis.Vertical, () =>
 		{
-			string menuName = menuInfo.name;
-			if (GUILayout.Button(menuName))
-			{
-				menuInfo.onClickMenu?.Invoke();
-			}
+			DrawSpace(20);
+
+			DrawPlayerSpawnPos();
+			DrawPlayerId();
+
+			DrawSpace(10);
+
+			DrawPlayerState();
+
+			DrawSpace(10);
+
+			DrawButton("플레이어 소환", 100, 20, SpawnPlayer);
+		});
+	}
+
+	private void DrawPlayerSpawnPos()
+	{
+		DrawLabel("생성할 플레이어 위치", 120, 20);
+
+		DrawAxis(Axis.Horizontal, () =>
+		{
+			float x = (float)DrawField(FieldType.Float, playerSpawnPos.x, 50, 20);
+			float y = (float)DrawField(FieldType.Float, playerSpawnPos.y, 50, 20);
+			float z = (float)DrawField(FieldType.Float, playerSpawnPos.z, 50, 20);
+
+			playerSpawnPos = new Vector3(x, y, z);
+		});
+	}
+
+	private void DrawPlayerId()
+	{
+		DrawLabel("플레이어 더미 ID", 100, 20);
+
+		DrawAxis(Axis.Horizontal, () =>
+		{
+			playerId = (int)DrawField(FieldType.Int, playerId, 50, 20);
+		});
+	}
+
+	private void DrawPlayerState()
+	{
+		ECreatureState currentState = ECreatureState.None;
+
+		if (currentPlayer != null)
+		{
+			currentState = currentPlayer.CreatureState;
+		}
+
+		DrawLabel($"플레이어 현재 스테이트 : [{currentState}]", 250, 20);
+	}
+
+	private void DrawButton(string buttonName, float width, float height, Action onClickButton)
+	{
+		if (GUILayout.Button(buttonName, GUILayout.Width(width), GUILayout.Height(height)))
+		{
+			onClickButton?.Invoke();
 		}
 	}
 
-	private static void OnClickTest()
+	private void DrawLabel(string label, float width, float height)
 	{
-		Debug.Log("테스트 버튼 입력");
+		EditorGUILayout.LabelField(label, GUILayout.Width(width), GUILayout.Height(height));
 	}
+
+	private object DrawField(FieldType fieldType, object oldValue, float width, float height)
+	{
+		switch(fieldType)
+		{
+			case FieldType.String:
+				return EditorGUILayout.TextField((string)oldValue, GUILayout.Width(width), GUILayout.Height(height));
+
+			case FieldType.Float:
+				return EditorGUILayout.FloatField((float)oldValue, GUILayout.Width(width), GUILayout.Height(height));
+
+			case FieldType.Int:
+				return EditorGUILayout.IntField((int)oldValue, GUILayout.Width(width), GUILayout.Height(height));
+
+			default:
+				return EditorGUILayout.TextField((string)oldValue, GUILayout.Width(width), GUILayout.Height(height));
+		}
+	}
+
+	private void DrawAxis(Axis type, Action onDrawLayout, float width = 0.0f, float height = 0.0f)
+	{
+		bool isValidWidthAndHeight = width > 0.01f && height > 0.01f;
+		switch (type)
+		{
+			case Axis.Horizontal:
+
+				if (isValidWidthAndHeight)
+				{
+					EditorGUILayout.BeginHorizontal(GUILayout.Width(width), GUILayout.Height(height));
+				}
+				else
+				{
+					EditorGUILayout.BeginHorizontal();
+				}
+
+				onDrawLayout?.Invoke();
+				EditorGUILayout.EndHorizontal();
+				break;
+
+			case Axis.Vertical:
+
+				if (isValidWidthAndHeight)
+				{
+					EditorGUILayout.BeginVertical(GUILayout.Width(width), GUILayout.Height(height));
+				}
+				else
+				{
+					EditorGUILayout.BeginVertical();
+				}
+
+				onDrawLayout?.Invoke();
+				EditorGUILayout.EndVertical();
+				break;
+		}
+	}
+
+	public void DrawScrollView(Vector2 centerPos, Action onDrawLayout, float width, float height)
+	{
+		EditorGUILayout.BeginScrollView(centerPos, GUILayout.Width(width), GUILayout.Height(height));
+
+		onDrawLayout?.Invoke();
+
+		EditorGUILayout.EndScrollView();	
+	}
+
+	public void DrawSpace(float width = 6.0f)
+	{
+		GUILayout.Space(width);
+	}
+
+	#region 버튼 콜백 및 인게임 함수
+
+	private void SpawnPlayer()
+	{
+		currentPlayer = Managers.Object.SpawnCreature<Player>(playerSpawnPos, playerId);
+	}
+
+	#endregion
+
+
 }
